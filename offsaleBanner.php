b42fbebd4236900e56e16bd8aaefecdc7bba1053
GamingDev27 <miko.gaming1975@gmail.com>
2026-03-28 21:58:59 +0800

    [Fixed] style.css call

--- offsaleBanner.php
+++ offsaleBanner.php
@@ -1,8 +1,10 @@
 <section class="offsale-banner">
-  <div class="offsale-content">
-    <h2 class="offsale-title">Off Sale</h2>
-    <p class="offsale-message">
-      This product is currently unavailable. Check back later for updates!
-    </p>
+  <div class="container">
+    <div class="offsale-content">
+      <h2 class="offsale-title">Off Sale</h2>
+      <p class="offsale-message">
+        This product is currently unavailable. Check back later for updates!
+      </p>
+    </div>
   </div>
 </section>
